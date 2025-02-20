94818bc8e7a9c488a65a73cd4ae0a98f727eb3cc
RFO <ricardo7k@yahoo.com.br>
2025-02-19 21:07:26 -0300

    Versao do GEMINI via .env

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ const router = Router();
 var GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 router.get('/name', async (req, res) => {    
     try {
